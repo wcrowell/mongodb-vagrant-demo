8ca69d2a463387dc9ba9ba6ab8faabd90865a2f1
William Crowell <william.crowell@roguewave.com>
2020-06-10 13:54:11 -0400

    Added provision and unprovision scripts and changed ssh user

---
+++ provision.sh
@@ -0,0 +1,26 @@
+date
+cd mongodb-east-prod1
+vagrant up
+cd ../mongodb-east-prod2
+vagrant up
+cd ../mongodb-east-prod3
+vagrant up
+#cd ../mongodb-central-prod1
+#vagrant up
+#cd ../mongodb-central-prod2
+#vagrant up
+#cd ../mongodb-central-prod3
+#vagrant up
+cd ../mongodb-west-prod1
+vagrant up
+cd ../mongodb-west-prod2
+vagrant up
+cd ../mongodb-west-prod3
+vagrant up
+cd ../mongodb-config-east-prod1
+vagrant up
+cd ../mongodb-config-central-prod1
+vagrant up
+cd ../mongodb-config-west-prod1
+vagrant up
+date
---
+++ unprovision.sh
@@ -0,0 +1,26 @@
+date
+cd mongodb-east-prod1
+vagrant destroy -f 
+cd ../mongodb-east-prod2
+vagrant destroy -f 
+cd ../mongodb-east-prod3
+vagrant destroy -f 
+#cd ../mongodb-central-prod1
+#vagrant destroy -f  
+#cd ../mongodb-central-prod2
+#vagrant destroy -f 
+#cd ../mongodb-central-prod3
+#vagrant destroy -f 
+cd ../mongodb-west-prod1
+vagrant destroy -f 
+cd ../mongodb-west-prod2
+vagrant destroy -f 
+cd ../mongodb-west-prod3
+vagrant destroy -f 
+cd ../mongodb-config-east-prod1
+vagrant destroy -f 
+cd ../mongodb-config-central-prod1
+vagrant destroy -f 
+cd ../mongodb-config-west-prod1
+vagrant destroy -f 
+date
